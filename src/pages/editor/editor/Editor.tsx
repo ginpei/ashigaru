46d0be17b0d0a09d540eab2889f0cf418d3c288d
Ginpei <ginpei@ginpei.dev>
2022-10-28 00:07:04 -0700

    feat: 🎸 spacing

--- src/pages/editor/editor/Editor.tsx
+++ src/pages/editor/editor/Editor.tsx
@@ -3,10 +3,10 @@ export interface EditorProps {
 
 export function Editor(): JSX.Element {
   return (
-    <div className="Editor grid grid-rows-[32px_auto] h-full">
-      <input className="h-8 text-3xl" type="text" placeholder="Title" />
+    <div className="Editor grid grid-rows-[2.25rem_auto] h-full">
+      <input className="h-9 px-4 text-3xl" type="text" placeholder="Title" />
       <textarea
-        className="resize-none"
+        className="p-4 resize-none"
         placeholder="Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid nobis amet illum illo doloribus quo aut. Eius fugiat mollitia illum excepturi repellat, commodi, quasi nihil facilis at eaque, deserunt iusto!"
       />
     </div>
