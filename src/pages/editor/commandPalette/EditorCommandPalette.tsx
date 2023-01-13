68e0e6793da6ea648d9b54dc92c6ae6939e5024d
Ginpei <ginpei@ginpei.dev>
2023-01-13 15:06:59 -0800

    fix: 🐛 filter commands

--- src/pages/editor/commandPalette/EditorCommandPalette.tsx
+++ src/pages/editor/commandPalette/EditorCommandPalette.tsx
@@ -50,14 +50,14 @@ export function EditorCommandPalette({
   return (
     <Dialog className="EditorCommandPallet" onClose={onDialogClose} open={open}>
       <div className="fixed inset-0 bg-black/30" aria-hidden="true" />
-      <div className="fixed inset-0 flex items-center justify-center p-4">
+      <div className="fixed w-full top-0 mx-auto flex items-center justify-center p-4">
         <Dialog.Panel className="w-full max-w-sm rounded bg-white">
           <Dialog.Title className="hidden">Command pallet</Dialog.Title>
           <form className="flex [&>*]:flex-1" onSubmit={onSubmit}>
             <NiceInput value={input} onChange={onInputChange} />
           </form>
           <ul className="CommandSuggestions">
-            {commands.map((command) => (
+            {filteredCommands.map((command) => (
               <CommandListItem
                 command={command}
                 key={command.id}
