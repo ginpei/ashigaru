bd19b1019f234d5d8e535b1acb516227a27b5494
Ginpei <ginpei@ginpei.dev>
2023-01-13 23:15:49 -0800

    refactor: 💡 better hook styles

--- src/domains/shortcut/focusHooks.ts
+++ src/domains/shortcut/focusHooks.ts
@@ -1,26 +1,14 @@
 import { useEffect, useState } from "react";
 
 export function useFocusTarget(d?: Document): string {
-  const [id, setId] = useState("");
-
-  useFocusElement((el) => {
-    const attrName = "data-focus-target";
-    const elTarget = el?.closest(`[${attrName}]`);
-    const newId = elTarget?.getAttribute(attrName) ?? "";
-    if (newId === id) {
-      return;
-    }
-
-    setId(newId);
-  }, d);
-
+  const el = useFocusElement(d);
+  const attrName = "data-focus-target";
+  const elTarget = el?.closest(`[${attrName}]`);
+  const id = elTarget?.getAttribute(attrName) ?? "";
   return id;
 }
 
-function useFocusElement(
-  callback: (el: Element | null) => void,
-  d?: Document
-): void {
+function useFocusElement(d?: Document): Element | null {
   const [elFocus, setElFocus] = useState<Element | null>(null);
 
   useEffect(() => {
@@ -42,7 +30,5 @@ function useFocusElement(
     return () => w.clearInterval(tm);
   }, [d, elFocus]);
 
-  useEffect(() => {
-    callback(elFocus);
-  }, [callback, elFocus]);
+  return elFocus;
 }
