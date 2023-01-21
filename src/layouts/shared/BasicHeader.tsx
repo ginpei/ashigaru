6a6e4ab8fe70b836f7284e7c91c2e4da7c04f6ea
Ginpei <ginpei@ginpei.dev>
2023-01-21 09:53:48 -0800

    feat: 🎸 basic header design

--- src/layouts/shared/BasicHeader.tsx
+++ src/layouts/shared/BasicHeader.tsx
@@ -6,7 +6,14 @@ export interface BasicHeaderProps {}
 
 export function BasicHeader(): JSX.Element {
   return (
-    <div className="BasicHeader">
+    <div
+      className={`
+      BasicHeader
+      bg-ginpei
+      [&_a]:text-white [&_a]:no-underline
+      [&_a:hover]:text-white [&_a:hover]:underline
+    `}
+    >
       <Container>
         <div className="inner">
           <NiceLink href={homePagePath()}>Home</NiceLink>
