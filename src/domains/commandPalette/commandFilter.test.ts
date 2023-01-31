def81a074ff3d7ddbdc23b4098ea982a0d16397d
Ginpei <ginpei@ginpei.dev>
2023-01-31 10:04:48 -0800

    test: 💍 highlightCommands

--- src/domains/commandPalette/commandFilter.test.ts
+++ src/domains/commandPalette/commandFilter.test.ts
@@ -1,5 +1,10 @@
 import { describe, expect, it } from "vitest";
-import { highlightFilteredCommandTitle } from "./commandFilter";
+import { CommandDefinition } from "../command/CommandDefinition";
+import {
+  CommandFilter,
+  highlightCommands,
+  highlightFilteredCommandTitle,
+} from "./commandFilter";
 
 describe("highlightFilteredCommandTitle()", () => {
   it("highlights matched characters", () => {
@@ -55,6 +60,55 @@ describe("highlightFilteredCommandTitle()", () => {
       { character: "o", highlight: false },
       { character: "l", highlight: true },
       { character: "o", highlight: true },
+    ]);
+  });
+});
+
+describe("highlightCommands", () => {
+  it("filters and highlights commands", () => {
+    const action = () => {};
+    const commands: CommandDefinition[] = [
+      {
+        action,
+        id: "command1",
+        title: "Command 1",
+      },
+      {
+        action,
+        id: "command2",
+        title: "Command 2",
+      },
+      {
+        action,
+        id: "command3",
+        title: "dd11",
+      },
+    ];
+    const filter: CommandFilter = {
+      keyword: "d1",
+    };
+
+    const result = highlightCommands(commands, filter);
+    expect(result.length).toBe(2);
+    expect(result[0].id).toBe("command1");
+    expect(result[1].id).toBe("command3");
+    expect(result[1].highlightedCharacters).toEqual([
+      {
+        character: "d",
+        highlight: true,
+      },
+      {
+        character: "d",
+        highlight: false,
+      },
+      {
+        character: "1",
+        highlight: true,
+      },
+      {
+        character: "1",
+        highlight: false,
+      },
     ]);
   });
 });
