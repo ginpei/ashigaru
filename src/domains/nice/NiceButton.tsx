e426731cab3fe5027a2e7bd9f9685db28fa03165
Ginpei <ginpei@ginpei.dev>
2023-02-16 18:18:11 -0800

    feat: 🎸 accept ref

--- src/domains/nice/NiceButton.tsx
+++ src/domains/nice/NiceButton.tsx
@@ -1,15 +1,17 @@
-import { ComponentPropsWithoutRef } from "react";
+import { ComponentPropsWithoutRef, forwardRef } from "react";
 
 export type NiceButtonProps = ComponentPropsWithoutRef<"button">;
 
-export function NiceButton({
-  className,
-  ...props
-}: NiceButtonProps): JSX.Element {
-  return (
-    <button
-      className={`NiceButton ${className} border-[1px] border-ginpei px-4 py-1 text-ginpei`}
-      {...props}
-    />
-  );
-}
+export const NiceButton = forwardRef<HTMLButtonElement, NiceButtonProps>(
+  ({ className, ...props }, ref): JSX.Element => {
+    return (
+      <button
+        className={`NiceButton ${className} border-[1px] border-ginpei px-4 py-1 text-ginpei`}
+        ref={ref}
+        {...props}
+      />
+    );
+  }
+);
+
+NiceButton.displayName = "NiceButton";
