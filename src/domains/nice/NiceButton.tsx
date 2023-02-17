8ad62de4e2b6e4a5cacbadb594e7865c75b87fa1
Ginpei <ginpei@ginpei.dev>
2023-02-16 18:46:02 -0800

    style: 💄 replace with predefined value

--- src/domains/nice/NiceButton.tsx
+++ src/domains/nice/NiceButton.tsx
@@ -6,7 +6,7 @@ export const NiceButton = forwardRef<HTMLButtonElement, NiceButtonProps>(
   ({ className, ...props }, ref): JSX.Element => {
     return (
       <button
-        className={`NiceButton ${className} border-[1px] border-ginpei px-4 py-1 text-ginpei`}
+        className={`NiceButton ${className} border border-ginpei px-4 py-1 text-ginpei`}
         ref={ref}
         {...props}
       />
