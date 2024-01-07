92cbf41074f3dcc6253db7b1a411c1c3cae303db
Ginpei <ginpei@ginpei.dev>
2024-01-07 03:27:25 -0800

    docs: ✏️ commands

---
+++ src/domains/command/showcase/index.tsx
@@ -0,0 +1,17 @@
+import { VStack } from "../../layout/VStack";
+import { NiceH1 } from "../../nice/NiceH";
+import { StraightLayout } from "../../pageLayout/straight/StraightLayout";
+import { CommandExample } from "./CommandExample";
+
+export function CommandIndex(): JSX.Element {
+  return (
+    <StraightLayout title="Command demos">
+      <VStack>
+        <NiceH1>Command examples</NiceH1>
+        <CommandExample />
+      </VStack>
+    </StraightLayout>
+  );
+}
+
+CommandIndex.path = "command" as const;
--- src/domains/showcase/showcaseList.ts
+++ src/domains/showcase/showcaseList.ts
@@ -1,8 +1,10 @@
+import { CommandIndex } from "../command/showcase";
 import { CommandPaletteShowcase } from "../commandPalette/showcase";
 import { MenuShowcase } from "../menu/showcase";
 import { ShortcutIndex } from "../shortcut/showcase";
 
 export const showcaseList = {
+  [CommandIndex.path]: CommandIndex,
   [CommandPaletteShowcase.path]: CommandPaletteShowcase,
   [MenuShowcase.path]: MenuShowcase,
   [ShortcutIndex.path]: ShortcutIndex,
