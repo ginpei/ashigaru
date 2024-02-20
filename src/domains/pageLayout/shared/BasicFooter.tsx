c4e1bd6c0886203424f34246b9bcdfab505dfe02
Ginpei <ginpei@ginpei.dev>
2024-02-19 23:53:48 -0800

    feat: 🎸 space on footer

--- src/domains/pageLayout/shared/BasicFooter.tsx
+++ src/domains/pageLayout/shared/BasicFooter.tsx
@@ -4,7 +4,12 @@ export interface BasicFooterProps {}
 
 export function BasicFooter(): JSX.Element {
   return (
-    <div className="BasicFooter border-t border-gray-500 border-dashed">
+    <div
+      className="
+      BasicFooter
+      mt-32 py-16 border-t border-dashed border-gray-500 text-gray-500
+      "
+    >
       <Container>BasicFooter</Container>
     </div>
   );
