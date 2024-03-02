93cb0b2ba6baa72603cdbcb6fea5be2c246af549
Ginpei <ginpei@ginpei.dev>
2024-03-02 01:35:41 -0800

    feat: 🎸 canvas frame style

--- src/pages/shaper/canvas/CanvasPane.tsx
+++ src/pages/shaper/canvas/CanvasPane.tsx
@@ -30,26 +30,32 @@ export function CanvasPane(): JSX.Element {
       <div
         className="
           CanvasPane
-          relative size-full bg-gray-50
+          relative size-full overflow-auto border bg-gray-50
           focus-within:bg-gray-200
           [:focus_&]:bg-gray-200
         "
         onMouseDown={onCanvasClick}
       >
-        <div
-          className="
-            absolute inset-0 m-2 border-4 border-gray-500 bg-white shadow-gray-900
-            focus-within:shadow
-            [:focus_&]:shadow
-          "
-          onMouseDown={onCanvasClick}
-        >
-          {shapes.map((data) => (
-            <ShapeDisplay key={data.id} onSelect={onShapeSelect} shape={data} />
-          ))}
-          {selectedShapes.map((data) => (
-            <Marquee key={data.id} onSelect={onShapeSelect} shape={data} />
-          ))}
+        <div className="absolute p-4">
+          <div
+            className="
+              relative border-4 border-gray-500 bg-white
+              shadow-[0_0_16px_4px] shadow-gray-500/50
+            "
+            onMouseDown={onCanvasClick}
+            style={{ height: "600px", width: "800px" }}
+          >
+            {shapes.map((data) => (
+              <ShapeDisplay
+                key={data.id}
+                onSelect={onShapeSelect}
+                shape={data}
+              />
+            ))}
+            {selectedShapes.map((data) => (
+              <Marquee key={data.id} onSelect={onShapeSelect} shape={data} />
+            ))}
+          </div>
         </div>
       </div>
     </FocusTarget>
