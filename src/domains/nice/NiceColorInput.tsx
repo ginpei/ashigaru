8f64aea45c8e157df89a441df534a521a293fccc
Ginpei <ginpei@ginpei.dev>
2024-03-02 02:29:55 -0800

    fix: 🐛 UI was collapsed

--- src/domains/nice/NiceColorInput.tsx
+++ src/domains/nice/NiceColorInput.tsx
@@ -9,7 +9,7 @@ export function NiceColorInput({
   return (
     <input
       className={`NiceColorInput ${className}
-        size-auto cursor-pointer border-[1px] border-transparent p-0
+        cursor-pointer border-[1px] border-transparent p-0
         invalid:border-red-500
         invalid:bg-red-100
         invalid:ring-red-500 read-only:bg-transparent
