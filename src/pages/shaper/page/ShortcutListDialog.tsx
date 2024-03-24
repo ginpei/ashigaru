65307ca7ec27dadb4314dcefce70fe2a3ca2b622
Ginpei <ginpei@ginpei.dev>
2024-03-24 11:55:39 -0700

    feat: 🎸 show shortcut list

--- src/pages/shaper/page/ShortcutListDialog.tsx
+++ src/pages/shaper/page/ShortcutListDialog.tsx
@@ -1,10 +1,11 @@
+import { useMemo, useState } from "react";
 import {
   Dialog,
   DialogBody,
-  DialogFooter,
   DialogHeader,
 } from "../../../domains/dialog/Dialog";
-import { NiceButton } from "../../../domains/nice/NiceButton";
+import { useShaperPageActions } from "../action/shaperPageActionHooks";
+import { ShaperPageState } from "./ShaperPageState";
 
 export interface ShortcutListDialogProps {
   onClose: () => void;
@@ -15,14 +16,54 @@ export function ShortcutListDialog({
   onClose,
   open,
 }: ShortcutListDialogProps): JSX.Element {
+  const list = useShortcutList();
+
   return (
     <Dialog onClose={onClose} open={open}>
       <DialogHeader onClose={onClose}>Shortcuts</DialogHeader>
       <DialogBody>
         <div className="w-96">
-          <p>TODO</p>
+          {list.map((v) => (
+            <div
+              key={`${v.commandId}--${v.when}--${v.key}`}
+              className="flex justify-between"
+            >
+              <div>{v.title}</div>
+              <div>{v.key}</div>
+              <div>{v.when ? `${v.when}` : <i>Global</i>}</div>
+            </div>
+          ))}
         </div>
       </DialogBody>
     </Dialog>
   );
+}
+
+function useShortcutList() {
+  const [state, setSate] = useState<ShaperPageState>(
+    null as unknown as ShaperPageState, // dummy
+  );
+  const [commands, shortcuts] = useShaperPageActions(state, setSate);
+
+  const list = useMemo(
+    () =>
+      shortcuts.map((shortcut) => {
+        const commandId = shortcut.commandId;
+        const command = commands.find((v) => v.id === commandId);
+        if (!command) {
+          throw new Error(`Command not found: ${commandId}`);
+        }
+
+        const item = {
+          commandId: commandId,
+          title: command.title,
+          key: shortcut.key,
+          when: shortcut.when,
+        };
+        return item;
+      }),
+    [commands, shortcuts],
+  );
+
+  return list;
 }
