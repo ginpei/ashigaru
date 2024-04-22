5745c0e335e9dc21287d57a9c9e4cdc006b71f11
Ginpei <ginpei@ginpei.dev>
2024-04-21 21:37:48 -0700

    docs: ✏️ update docs

--- src/domains/action/_showcase/aboutAction/index.tsx
+++ src/domains/action/_showcase/aboutAction/index.tsx
@@ -1,6 +1,7 @@
 import { VStack } from "../../../layout/VStack";
 import { NiceCode } from "../../../nice/NiceCode";
-import { NiceH1 } from "../../../nice/NiceH";
+import { NiceCodeBlock } from "../../../nice/NiceCodeBlock";
+import { NiceH1, NiceH2 } from "../../../nice/NiceH";
 import { StraightLayout } from "../../../pageLayout/straight/StraightLayout";
 
 export function ActionAboutActionDemoPage(): JSX.Element {
@@ -8,25 +9,87 @@ export function ActionAboutActionDemoPage(): JSX.Element {
     <StraightLayout title="About action">
       <VStack>
         <NiceH1>About action</NiceH1>
-        <ul className="ui-ul">
+        <NiceH2>Prepare</NiceH2>
+        <ol className="ui-ol">
           <li>
-            <NiceCode>CommandDefinition</NiceCode> - Command ID, function, etc.
+            Define actions, which type is <NiceCode>Action</NiceCode>
           </li>
           <li>
-            <NiceCode>KeyboardShortcut</NiceCode> - Key combination, command ID
-            to invoke, etc.
+            Break down the actions into commands and shortcuts using{" "}
+            <NiceCode>breakActions()</NiceCode>, which returns{" "}
+            <NiceCode>[CommandDefinition[], KeyboardShortcut[]]</NiceCode>
+          </li>
+        </ol>
+        <NiceCodeBlock>
+          {`
+const actions: Action[] = [
+  {
+    exec(args) {
+      console.log("executed", args);
+    },
+    id: "action1",
+    shortcuts: [
+      {
+        args: [1, 2],
+        key: "Ctrl+X",
+        when: "focus:canvas",
+      },
+    ],
+    title: "An example action",
+  },
+];
+
+const [commands, shortcuts] = breakActions(actions);
+        `.trim()}
+        </NiceCodeBlock>
+        <NiceH2>Run a command by ID</NiceH2>
+        <ol className="ui-ol">
+          <li>
+            Pick a command by an ID using{" "}
+            <NiceCode>pickCommandDefinition()</NiceCode>
           </li>
           <li>
-            <NiceCode>Action</NiceCode> - <NiceCode>CommandDefinition</NiceCode>{" "}
-            + list of default <NiceCode>KeyboardShortcut</NiceCode>
+            Run the command using <NiceCode>command.exec()</NiceCode>
+          </li>
+        </ol>
+        <NiceCodeBlock>
+          {`
+const command = pickCommandDefinition(commands, "action1");
+command.exec();
+        `.trim()}
+        </NiceCodeBlock>
+        <NiceH2>Run a command by keyboard shortcut</NiceH2>
+        <ol className="ui-ol">
+          <li>
+            Observe user keyboard input events, and get a keyboard input key
+            (e.g. <NiceCode>&quot;Ctrl+Shift+X&quot;</NiceCode>) from the event
+            using <NiceCode>keyboardEventToInputCommand()</NiceCode>
           </li>
           <li>
-            Use <NiceCode>buildActions()</NiceCode> to build{" "}
-            <NiceCode>CommandDefinition</NiceCode> and
-            <NiceCode>KeyboardShortcut</NiceCode> from{" "}
-            <NiceCode>Action</NiceCode>
+            Pick a shortcut by input key combination and conditions using{" "}
+            <NiceCode>pickShortcutDefinition()</NiceCode>
           </li>
-        </ul>
+          <li>
+            Pick a command by an command ID on the shortcut using{" "}
+            <NiceCode>pickCommandDefinition()</NiceCode>
+          </li>
+          <li>
+            Run the command using <NiceCode>command.exec()</NiceCode>
+          </li>
+        </ol>
+        <NiceCodeBlock>
+          {`
+const key = keyboardEventToInputCommand(keyboardEvent);
+
+const conditions: ConditionFunctionMap = {
+  "focus:canvas": createConditionFunction("", "focus:canvas", () => true),
+};
+
+const shortcut = pickShortcutDefinition(shortcuts, conditions, key);
+const command = pickCommandDefinition(commands, shortcut.commandId);
+command.exec(...(shortcut.args ?? []));
+        `.trim()}
+        </NiceCodeBlock>
       </VStack>
     </StraightLayout>
   );
