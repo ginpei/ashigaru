714622663eddba93183a4223473b667856a08803
Ginpei <ginpei@ginpei.dev>
2024-05-20 01:03:00 -0700

    feat: 🎸 condition parser

--- src/domains/action/Condition.test.ts
+++ src/domains/action/Condition.test.ts
@@ -3,6 +3,7 @@ import {
   ConditionFunctionMap,
   createConditionFunction,
   doesConditionMatch,
+  parseConditionString,
 } from "./Condition";
 
 describe("doesConditionMatch()", () => {
@@ -34,5 +35,52 @@ describe("doesConditionMatch()", () => {
     const result = doesConditionMatch("isOk", conditions);
     expect(result).toBe(false);
     expect(fn).not.toBeCalled();
+  });
+});
+
+describe("parseConditionString()", () => {
+  it("returns multiple tokens with symbols", () => {
+    const expression = "foo && !bar || boo:arg && !name_space.prop:arg1,arg_2";
+    const result = parseConditionString(expression);
+
+    expect(result).toEqual([
+      { type: "function", key: "foo", negative: false },
+      { type: "operator", key: "&&" },
+      { type: "function", key: "bar", negative: true },
+      { type: "operator", key: "||" },
+      { type: "function", key: "boo", negative: false, arg: "arg" },
+      { type: "operator", key: "&&" },
+      {
+        type: "function",
+        key: "name_space.prop",
+        negative: true,
+        arg: "arg1,arg_2",
+      },
+    ]);
+  });
+
+  it("does not require spaces", () => {
+    const expression = "foo&&!bar:arg";
+    const result = parseConditionString(expression);
+
+    expect(result).toEqual([
+      { type: "function", key: "foo", negative: false },
+      { type: "operator", key: "&&" },
+      { type: "function", key: "bar", negative: true, arg: "arg" },
+    ]);
+  });
+
+  it("throws for operator without expression", () => {
+    expect(() => {
+      const expression = "foo&&";
+      const result = parseConditionString(expression);
+    }).toThrow("Invalid operator pair");
+  });
+
+  it("throws for invalid operator pairs", () => {
+    expect(() => {
+      const expression = "foo&&||";
+      const result = parseConditionString(expression);
+    }).toThrow("Invalid operator order");
   });
 });
--- src/domains/action/Condition.ts
+++ src/domains/action/Condition.ts
@@ -7,6 +7,20 @@ export interface ConditionFunctionMap {
   [key: string]: ConditionFunction;
 }
 
+export type ConditionToken = ConditionFunctionToken | ConditionOperatorToken;
+
+export interface ConditionFunctionToken {
+  arg: string;
+  key: string;
+  negative: boolean;
+  type: "function";
+}
+
+export interface ConditionOperatorToken {
+  key: "&&" | "||";
+  type: "operator";
+}
+
 export function createConditionFunction(
   source: string,
   key: string,
@@ -33,4 +47,72 @@ export function doesConditionMatch(
 
   const matched = doesConditionMatch();
   return matched;
+}
+
+export function parseConditionString(condition: string): ConditionToken[] {
+  // TODO rename
+
+  const conditions: ConditionToken[] = [];
+  for (const expression of tokenize(condition)) {
+    const token = readToken(expression);
+    conditions.push(token);
+  }
+  return conditions;
+}
+
+/**
+ * @example
+ * tokenize("foo"); // ["foo"]
+ * tokenize("!foo"); // ["!foo"]
+ * tokenize("foo:arg"); // ["foo:arg"]
+ * tokenize("foo&&bar || baz"); // ["foo", "&&", "bar", "||", "baz"]
+ */
+function tokenize(expression: string) {
+  const rxToken = /(?:[!]?[\w.]+(?::[\w,]+)?|&&|\|\|)/g;
+  const tokens = expression.match(rxToken);
+  if (!tokens) {
+    throw new Error(`Invalid condition string: ${expression}`);
+  }
+
+  if (tokens.length % 2 === 0) {
+    throw new Error(`Invalid operator pair: ${expression}`);
+  }
+
+  const orderCorrect = tokens.every((token, index) => {
+    if (index % 2 === 0) {
+      return token !== "&&" && token !== "||";
+    }
+    return token === "&&" || token === "||";
+  });
+  if (!orderCorrect) {
+    throw new Error(`Invalid operator order: ${expression}`);
+  }
+
+  return tokens;
+}
+
+function readToken(expression: string): ConditionToken {
+  // break down condition function
+  // "!foo:bar" => ["!foo:bar", "!", "foo", "bar"]
+  const rxToken = /^(!?)((?:\w|\.)*)(?::([\w,]+))?$/;
+
+  if (expression === "&&" || expression === "||") {
+    return {
+      key: expression as "&&" | "||",
+      type: "operator",
+    };
+  }
+
+  const tokens = expression.match(rxToken);
+  if (!tokens) {
+    throw new Error(`Invalid condition token: ${expression}`);
+  }
+
+  const [, sNegative, key, arg] = tokens;
+  return {
+    arg,
+    key,
+    negative: sNegative === "!",
+    type: "function",
+  };
 }
