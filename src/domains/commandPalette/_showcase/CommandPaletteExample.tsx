83acae6d6824e6fd6221b8c18a20250e010d8774
Ginpei <ginpei@ginpei.dev>
2024-05-25 17:31:14 -0700

    docs: ✏️ update examples

--- src/domains/commandPalette/_showcase/CommandPaletteExample.tsx
+++ src/domains/commandPalette/_showcase/CommandPaletteExample.tsx
@@ -1,65 +1,56 @@
-import { Dispatch, SetStateAction, useState } from "react";
+import { useState } from "react";
 import { EditorCommandPalette } from "../../../pages/editor/actions/EditorCommandPalette";
-import {
-  createEditorPageState,
-  EditorPageState,
-} from "../../../pages/editor/pageState/EditorPageState";
+import { EditorActionContextProvider } from "../../../pages/editor/actions/editorActionContext";
+import { createEditorPageState } from "../../../pages/editor/pageState/EditorPageState";
 import { EditorPageStateProvider } from "../../../pages/editor/pageState/editorPageStateContext";
 import { breakActions } from "../../action/Action";
+import { ConditionFunctionMap } from "../../action/Condition";
+import { useShortcutRunner } from "../../action/keyboardShortcutHooks";
 import { HStack } from "../../layout/HStask";
 import { NiceButton } from "../../nice/NiceButton";
 import { NiceH2 } from "../../nice/NiceH";
-import { Note } from "../../note/Note";
-import { HighlightedCommand } from "../commandFilter";
 import { demoActions, demoNotes } from "./exampleResources";
-
-const [demoCommands, demoShortcuts] = breakActions(demoActions);
 
 export function CommandPaletteExample() {
   const [state, setState] = useState(
-    createEditorPageState({
-      notes: demoNotes,
-      commands: demoCommands, // TODO remove
-      shortcuts: demoShortcuts, // TODO remove
-    }),
+    createEditorPageState({ notes: demoNotes }),
   );
 
-  const onCommandSelect = (
-    command:
-      | Note
-      | HighlightedCommand<
-          [EditorPageState, Dispatch<SetStateAction<EditorPageState>>]
-        >
-      | null,
-  ) => {
-    console.log("# command", command);
+  const [commands, shortcuts] = breakActions(demoActions);
+  const conditions: ConditionFunctionMap = {};
+
+  useShortcutRunner(commands, shortcuts, conditions);
+
+  const onCommandSelect = () => {
     setState((v) => ({ ...v, commandPaletteVisible: "" }));
   };
 
   return (
     <EditorPageStateProvider value={[state, setState]}>
-      <NiceH2>&lt;EditorCommandPalette&gt;</NiceH2>
-      <p>* No shortcuts are prepared in this demo page.</p>
-      <HStack>
-        <NiceButton
-          onClick={() =>
-            setState((v) => ({ ...v, commandPaletteVisible: "commands" }))
-          }
-        >
-          Open
-        </NiceButton>
-        <NiceButton
-          onClick={() =>
-            setState((v) => ({ ...v, commandPaletteVisible: "files" }))
-          }
-        >
-          Select file...
-        </NiceButton>
-      </HStack>
-      <EditorCommandPalette
-        open={state.commandPaletteVisible}
-        onClose={onCommandSelect}
-      />
+      <EditorActionContextProvider value={[commands, shortcuts]}>
+        <NiceH2>&lt;EditorCommandPalette&gt;</NiceH2>
+        <p>* No shortcuts are prepared in this demo page.</p>
+        <HStack>
+          <NiceButton
+            onClick={() =>
+              setState((v) => ({ ...v, commandPaletteVisible: "commands" }))
+            }
+          >
+            Open
+          </NiceButton>
+          <NiceButton
+            onClick={() =>
+              setState((v) => ({ ...v, commandPaletteVisible: "files" }))
+            }
+          >
+            Select file...
+          </NiceButton>
+        </HStack>
+        <EditorCommandPalette
+          open={state.commandPaletteVisible}
+          onClose={onCommandSelect}
+        />
+      </EditorActionContextProvider>
     </EditorPageStateProvider>
   );
 }
