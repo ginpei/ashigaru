a03a1b634c1989417a9f9d4d10ace4543eb7834b
Ginpei <ginpei@ginpei.dev>
2024-06-09 07:21:40 +0900

    feat: 🎸 nice section

---
+++ src/domains/nice/NiceSection.tsx
@@ -0,0 +1,42 @@
+import { ComponentPropsWithoutRef, ReactNode, forwardRef } from "react";
+import { NiceH1, NiceH2, NiceH3 } from "./NiceH";
+
+export type NiceSectionProps = ComponentPropsWithoutRef<"section"> & {
+  as?: "section" | "article" | "div";
+  heading: ReactNode;
+  level: "1" | "2" | "3";
+};
+
+const HeadingMap = {
+  1: NiceH1,
+  2: NiceH2,
+  3: NiceH3,
+} as const;
+
+const gapMap = {
+  1: "gap-16",
+  2: "gap-8",
+  3: "gap-4",
+} as const;
+
+export const NiceSection = forwardRef<HTMLDivElement, NiceSectionProps>(
+  (
+    { as: Tag = "section", className, children, level, ...props },
+    ref,
+  ): JSX.Element => {
+    const H = HeadingMap[level];
+    const gapClass = gapMap[level];
+    return (
+      <Tag
+        className={`${className} NiceSection flex flex-col ${gapClass}`}
+        ref={ref}
+        {...props}
+      >
+        <H>{props.heading}</H>
+        {children}
+      </Tag>
+    );
+  },
+);
+
+NiceSection.displayName = "NiceSection";
